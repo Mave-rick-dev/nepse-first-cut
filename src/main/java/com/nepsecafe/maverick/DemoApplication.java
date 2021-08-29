664b4c76efaeed74c1102d53c33e6a87795387ff
mave-rick-dev <sangam.ale.sa63@gmail.com>
2021-08-29 19:34:47 +0545

    configured postgres

--- src/main/java/com/nepsecafe/maverick/DemoApplication.java
+++ src/main/java/com/nepsecafe/maverick/DemoApplication.java
@@ -5,17 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@GetMapping
-	public String hello(){
-		return "hello world";
 	}
 }
 
